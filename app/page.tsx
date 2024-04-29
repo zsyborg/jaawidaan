1a79b6c806fff7281ad6c44b24cabfd5cf7c884a
Zasha <zahid.shaikh@live.com>
2024-04-29 11:56:48 +0530

    Stately

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ function Home() {
       {/* Page intro text */}
       <div className="content-1140 header-content center-relative block">
         <h1 className="entry-title">
-          Elegant & Statestly, <br/>Buffetware, Tableware, Decor<br/> made with <i className="fa fas fa-heart" style={{color:"red"}} /> Bombay, India.<br/> Creating & Crafting<br/> Circa, 2000
+          Elegant & Stately, <br/>Buffetware, Tableware, Decor<br/> made with <i className="fa fas fa-heart" style={{color:"red"}} /> Bombay, India.<br/> Creating & Crafting<br/> Circa, 2000
         </h1>
         {/* <p className="page-desc">PETER WALL + COCO BASIC</p> */}
       </div>
